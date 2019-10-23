f86051630221e26a047c0cac07aa660321509eec
alex-held <git@alexanderheld.net>
2019-10-23 16:45:29 +0200

    docs: add xml-comments

--- src/HardCoded.MockServer.Contracts/Attributes/WhitelistAssemblyForReflectionAttribute.cs
+++ src/HardCoded.MockServer.Contracts/Attributes/WhitelistAssemblyForReflectionAttribute.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Reflection;
 
 
 namespace HardCoded.MockServer.Contracts.Attributes
 {
+    /// <summary>
+    /// Marks an <see cref="Assembly"/> 
+    /// </summary>
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
     public class WhiteListAssemblyReflectionAttribute : Attribute { }
 }
