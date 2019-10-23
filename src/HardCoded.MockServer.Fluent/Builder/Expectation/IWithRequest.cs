0255144cc365533f1f6b6ff19405354b9805498e
alex-held <git@alexanderheld.net>
2019-10-24 01:40:19 +0200

    feat: Setups can be configured to only be valid for one time

--- src/HardCoded.MockServer.Fluent/Builder/Expectation/IWithRequest.cs
+++ src/HardCoded.MockServer.Fluent/Builder/Expectation/IWithRequest.cs
@@ -12,7 +12,10 @@ namespace HardCoded.MockServer.Fluent.Builder.Expectation
     [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IWithRequest: IFluentInterface
     {
-        IWithResponse RespondWith(int statusCode, Action<IFluentHttpResponseBuilder> responseFactory);
-        IWithResponse RespondWith(HttpStatusCode statusCode, Action<IFluentHttpResponseBuilder> responseFactory);
+        IWithResponse RespondWith(int statusCode, Action<IFluentHttpResponseBuilder> responseFactory = null);
+        IWithResponse RespondWith(HttpStatusCode statusCode, Action<IFluentHttpResponseBuilder> responseFactory = null);
+        
+        IWithResponse RespondOnce(int statusCode, Action<IFluentHttpResponseBuilder> responseFactory = null);
+        IWithResponse RespondOnce(HttpStatusCode statusCode, Action<IFluentHttpResponseBuilder> responseFactory = null);
     }
 }
