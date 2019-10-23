7f611951e5445f737e4ffc86a42f6280102c0395
alex-held <git@alexanderheld.net>
2019-10-24 00:35:21 +0200

    refactor: befriend TestContainers.tests assembly

--- src/HardCoded.MockServer.Contracts/AssemblyInfo.cs
+++ src/HardCoded.MockServer.Contracts/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.CompilerServices;
 
 
 [assembly: InternalsVisibleTo("HardCoded.MockServer.Fluent.Tests")]
-[assembly: InternalsVisibleTo("HardCoded.MockServer.TestContainers")]
+[assembly: InternalsVisibleTo("HardCoded.MockServer.TestContainers.Tests")]
 [assembly: InternalsVisibleTo("HardCoded.MockServer.Tests")]
