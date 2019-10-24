e5cf3e02bc942b364a35722ba9f8a4a209ce5473
alex-held <git@alexanderheld.net>
2019-10-24 03:26:34 +0200

    chore: cleanup

--- src/HardCoded.MockServer.Contracts/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("NinjaTools.FluentMockServer")]
-[assembly: InternalsVisibleTo("NinjaTools.FluentMockServer.Contracts")]
-[assembly: InternalsVisibleTo("NinjaTools.FluentMockServer.TestContainers")]
-[assembly: InternalsVisibleTo("NinjaTools.FluentMockServer.TestContainers.Tests")]
-[assembly: InternalsVisibleTo("NinjaTools.FluentMockServer.Tests")]
