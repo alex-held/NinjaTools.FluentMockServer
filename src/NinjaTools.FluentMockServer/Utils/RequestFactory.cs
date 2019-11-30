f63c15d6896c7e69c7c51e74efd691a67599737a
alex-held <git@alexanderheld.net>
2019-11-30 11:36:45 +0100

    feat(test-container): add factory for MockServerAPI-Requests

---
+++ src/NinjaTools.FluentMockServer/Utils/RequestFactory.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using NinjaTools.FluentMockServer.Models;
+using NinjaTools.FluentMockServer.Models.HttpEntities;
+using NinjaTools.FluentMockServer.Utils;
+
+namespace NinjaTools.FluentMockServer.Requests
+{
+    public static class MockServerEndpoints
+    {
+        public const string Expectations = "mockserver/expectation";
+        public const string Reset  = "mockserver/reset";
+        public const string Verify  = "mockserver/verify";
+    }
+
+    public static class RequestFactory
+    {
+        public static HttpRequestMessage Reset() => new HttpRequestMessage(HttpMethod.Put, MockServerEndpoints.Reset);
+        
+        public static HttpRequestMessage Verify(Verify verify) =>  new HttpRequestMessage(HttpMethod.Put, MockServerEndpoints.Verify)
+        {
+            Content = new JsonContent(verify)
+        };
+        
+        public static HttpRequestMessage Expectation(Expectation expectation) =>  new HttpRequestMessage(HttpMethod.Put, MockServerEndpoints.Expectations)
+        {
+            Content = new JsonContent(expectation)
+        };
+        
+    }
+ 
+}
