a25a377ba3ca45c463deb423a94ade4dd2f19652
alex-held <git@alexanderheld.net>
2019-12-01 12:00:56 +0100

    refactor(api): cleanup obsolete commented code

--- src/NinjaTools.FluentMockServer.API/Data/ExpectationDBContext.cs
+++ src/NinjaTools.FluentMockServer.API/Data/ExpectationDBContext.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using NinjaTools.FluentMockServer.Domain.Models;
 
@@ -8,30 +9,17 @@ namespace NinjaTools.FluentMockServer.API.Data
     
     public class ExpectationDbContext : DbContext
     {
-        public ExpectationDbContext(DbContextOptions<ExpectationDbContext> builder) : base(builder) { }
+        public ExpectationDbContext([NotNull] DbContextOptions builder) : base(builder) { }
         
         public DbSet<Expectation> Expectations { get; set; }
-//        public DbSet<HttpRequest> Requests { get; set; }
-//        public DbSet<HttpForward> Forwards { get; set; }
-//        public DbSet<HttpTemplate> Templates { get; set; }
-//        public DbSet<HttpError> Errors { get; set; }
-//        public DbSet<Times> Times { get; set; }  
-//        public DbSet<LifeTime> LifeTimes { get; set; }  
-//        public DbSet<Verify> Verifies { get; set; }  
-//        
+
 
         /// <inheritdoc />
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ExpectationDbContext).Assembly);
         }
-
-        /// <inheritdoc />
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
-
+        
         /// <inheritdoc />
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
