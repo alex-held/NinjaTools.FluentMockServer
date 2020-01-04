538f3ad3475897ef77008ec2889e8e226dace817
alex held <git@alexanderheld.net>
2020-01-04 19:00:34 +0100

    styles: apply ReSharper code styles

--- src/NinjaTools.FluentMockServer.API/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/NinjaTools.FluentMockServer.API/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -5,12 +6,14 @@ namespace NinjaTools.FluentMockServer.API.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IMockServerBuilder AddMockServer(this IServiceCollection services, IConfiguration configuration)
+        [NotNull]
+        public static IMockServerBuilder AddMockServer([NotNull] this IServiceCollection services, [NotNull] IConfiguration configuration)
         {
             return new MockServerBuilder(services, configuration);
         }
 
-        public static IMockServerBuilder AddMockServer(this IServiceCollection services)
+        [NotNull]
+        public static IMockServerBuilder AddMockServer([NotNull] this IServiceCollection services)
         {
             var config = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
             return AddMockServer(services, config);
--- src/NinjaTools.FluentMockServer.API/Extensions/HttpContextExtensions.cs
+++ src/NinjaTools.FluentMockServer.API/Extensions/HttpContextExtensions.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Linq;
 
@@ -7,7 +8,7 @@ namespace NinjaTools.FluentMockServer.API.Extensions
 {
     public static class HttpContextExtensions
     {
-        public static async Task<T> ReadAsync<T>(this HttpRequest httpRequest)
+        public static async Task<T> ReadAsync<T>([NotNull] this HttpRequest httpRequest)
         {
             var reader = new StreamReader(httpRequest.Body);
             var json = await reader.ReadToEndAsync();
