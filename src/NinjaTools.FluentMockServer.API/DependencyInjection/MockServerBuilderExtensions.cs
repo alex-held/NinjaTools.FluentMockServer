592dec13897250dba2f6c2974852647e3749bbd6
alex held <git@alexanderheld.net>
2020-01-04 19:29:59 +0100

    refactor(mock-server): use ports 80 (matching) + 1080 (admin)

--- src/NinjaTools.FluentMockServer.API/DependencyInjection/MockServerBuilderExtensions.cs
+++ src/NinjaTools.FluentMockServer.API/DependencyInjection/MockServerBuilderExtensions.cs
@@ -7,7 +7,7 @@ namespace NinjaTools.FluentMockServer.API.DependencyInjection
     public static class MockServerBuilderExtensions
     {
         [NotNull]
-        public static IMockServerBuilder AddAdminPath([NotNull] this IMockServerBuilder builder, int port = 2081)
+        public static IMockServerBuilder AddAdminPath([NotNull] this IMockServerBuilder builder, int port = 1080)
         {
             builder.Services.Configure<AdminOptions>(opt => { opt.Port = port; });
 
